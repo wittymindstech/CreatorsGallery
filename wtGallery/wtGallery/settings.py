c82705936f4e483b713307e6c26065d3a63e8b54
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-07-23 17:01:02 +0530

    Update settings.py

--- wtGallery/wtGallery/settings.py
+++ wtGallery/wtGallery/settings.py
@@ -140,7 +140,7 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
      'CacheControl': 'max-age=86400',
 }
-DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'wtGallery.storage_backends.MediaStorage'
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
