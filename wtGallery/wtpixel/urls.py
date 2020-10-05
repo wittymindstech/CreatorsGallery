0175f96fe25841d2de7d73df6a70b89d2e72925a
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-10-05 19:42:00 +0530

    Update Layout and Add User Authentication

--- wtGallery/wtpixel/urls.py
+++ wtGallery/wtpixel/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
-    # path('login/', views.login_view, name='login'),
-    # path('register/', views.register, name='register'),
-    path('search/', views.SearchResultsView.as_view(), name='search_results'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    path('register/', views.register, name='register'),
+    path('search/', views.SearchResultsView.as_view(), name='search'),
+    # path('search/', views.search, name='search'),
 ]
 
 if settings.DEBUG:
--- wtGallery/wtpixel/views.py
+++ wtGallery/wtpixel/views.py
@@ -10,48 +10,65 @@ from django.views.generic import ListView
 from wtpixel.forms import ImageForm, SignUpForm, LoginForm
 from django.contrib import messages
 from wtpixel.models import Image
+import pyrebase
+from .documents import ImageDocument
 
-# @login_required(login_url="/login/")
+config = {
+    'apiKey': "AIzaSyDaJEJ9ZM1lvMYUWEfBORtDKQwnQ822kl0",
+    'authDomain': "wtgallery-f7b39.firebaseapp.com",
+    'databaseURL': "https://wtgallery-f7b39.firebaseio.com",
+    'projectId': "wtgallery-f7b39",
+    'storageBucket': "wtgallery-f7b39.appspot.com",
+    'messagingSenderId': "614208620267",
+    'appId': "1:614208620267:web:69be8e488eeb0c7ae4a0ae",
+    'measurementId': "G-TXRPVZFW7M"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+
+
 def index(request):
     portfolio = Image.objects.all()
     context = {"portfolio": portfolio}
     return render(request, "index.html", context)
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         form = SignUpForm()
-#     return render(request, "register.html", {'form': form})
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, "register.html", {'form': form})
 
 
-# def login_view(request):
-#     form = LoginForm(request.POST or None)
-#     msg = None
-#     if request.method == "POST":
-#         if form.is_valid():
-#             username = form.cleaned_data.get("username")
-#             password = form.cleaned_data.get("password1")
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect("/")
-#             else:
-#                 msg = "Username or Password Doesn't match"
-#         else:
-#             msg = 'Error validating the form'
-#     return render(request, "login.html", {"form": form, "msg": msg})
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == "POST":
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = "Username or Password Doesn't match"
+        else:
+            msg = 'Error validating the form'
+    return render(request, "login.html", {"form": form, "msg": msg})
 
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def upload(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
@@ -66,7 +83,7 @@ def upload(request):
 
 class SearchResultsView(ListView):
     model = Image
-    template_name = 'search_results.html'
+    template_name = 'search.html'
     context_object_name = 'all_search_results'
 
     def get_queryset(self):
@@ -74,7 +91,17 @@ class SearchResultsView(ListView):
         object_list = Image.objects.filter(Q(title__icontains=query) | Q(image__icontains=query))
         return object_list
 
-# @login_required(login_url="/login/")
+# def search(request):
+#     q = request.GET.get('q')
+#     if q:
+#         image = ImageDocument.search().query("match", title=q)
+#     else:
+#         image = ''
+#
+#     return render(request, 'search.html', {'image': image})
+
+
+@login_required(login_url="/login/")
 def pages(request):
     context = {}
     try:
